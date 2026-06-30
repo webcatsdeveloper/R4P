578132e93c8ee54c60571a381da3bf134babbaa7
haritha-kumaran <harithalakshmi2005@gmail.com>
2026-06-30 12:54:54 +0530

    Clean up temporary test script

--- test_connect.js
+++
@@ -1,7 +0,0 @@
-const https = require('https');
-
-https.get('https://right4p.myshopify.com/admin/api/2026-04/graphql.json', (res) => {
-  console.log('Status Code:', res.statusCode);
-}).on('error', (err) => {
-  console.error('Error:', err.message);
-});
